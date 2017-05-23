1f0ee5fcee37644f4fb469f5a6c12a1620ee92a1
kHRYSTAL <723526676@qq.com>
2017-05-23 17:41:14 +0800

    update

--- SlidingArcView/library/src/main/java/me/khrystal/widget/slidearcview/SlidingArcView.java
+++ SlidingArcView/library/src/main/java/me/khrystal/widget/slidearcview/SlidingArcView.java
@@ -3,10 +3,14 @@ package me.khrystal.widget.slidearcview;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.os.Build;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,11 +30,16 @@ public class SlidingArcView extends ViewGroup {
 
     private static String TAG = SlidingArcView.class.getSimpleName();
 
-    // 需要外部 set 的数据
+    //region 需要外部 set 的数据
     private List<String> mTitles;
     private List<Integer> mSrcList;
+    private List<SignView> views = new ArrayList<>();
+    Bitmap mBackgroundBitmap;
+    Paint mPaint = new Paint();
+    int mDiameter;
+    //endregion
 
-    // 与内部子 view 相关的属性
+    //region 与内部子 view 相关的属性
     private boolean isAnimated = false; // 动画是否在执行
     private int viewTopChange = ScreenUtil.dp2px(80f); // 子view向上偏移的位移
     private VelocityTracker mVelocityTracker; // fling 使用
@@ -38,20 +47,21 @@ public class SlidingArcView extends ViewGroup {
     private SignView leftView; // 屏幕最左边的子view
     private SignView rightView; // 屏幕最右边的子view
     // 外层圆心
-    private int CentX;
-    private int CentY;
+    private int mCenterX;
+    private int mCenterY;
 
-    private int RADIUS; // 半径
+    private int mRadius; // 半径
     private static final int VX = 50; // 第一个view的x坐标
     private SignView chooseView; // 选中的view;
     private SignView lastChooseView;
     private boolean isClicke = false;
+    //endregion
 
-    private List<SignView> views = new ArrayList<>();
-    Bitmap bgBitmap;
-    int mSize;
-
-
+    private boolean supportScroll = true;
+    private int lastX;
+    private int downPointId;
+    private int downX;
+    private int downY;
 
 
     public SlidingArcView(Context context) {
@@ -65,7 +75,6 @@ public class SlidingArcView extends ViewGroup {
     public SlidingArcView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
-
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -74,25 +83,82 @@ public class SlidingArcView extends ViewGroup {
         init();
     }
 
-    private void init() {
-//        bgBitmap = ((BitmapDrawable) getResources().getDrawable())
-        CentX = ScreenUtil.getScreenW() / 2;
-        RADIUS = ScreenUtil.getScreenW() / 2 + 100;
-        for (int i = 0; i < mSrcList.size(); i++) {
-
-        }
-    }
-
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        // do nothing
+    }
 
+    private void init() {
+        // TODO 背景图片
+        // TODO: 17/5/23  弧形圆心
+        mCenterX = ScreenUtil.getScreenW() / 2;
+        // TODO: 17/5/23   弧形半径
+        mRadius = ScreenUtil.getScreenW() / 2 + 100;
+        setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        setClickable(true);
+
+        // TODO: 17/5/23 添加子View
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        mSize = Math.min(getMeasuredWidth(), getMeasuredHeight());
-        setMeasuredDimension(mSize, mSize);
+        mDiameter = Math.min(getMeasuredWidth(), getMeasuredHeight());
+        setMeasuredDimension(mDiameter, mDiameter);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mBackgroundBitmap != null)
+            drawBackground(canvas);
+    }
+
+    /**
+     * 绘制弧形背景
+     * @param canvas
+     */
+    private void drawBackground(Canvas canvas) {
+        Bitmap bitmap = Bitmap.createScaledBitmap(mBackgroundBitmap, mDiameter, mDiameter, false);
+        if (bitmap == null)
+            return;
+        BitmapShader mBitmapShader = new BitmapShader(bitmap, Shader.TileMode.REPEAT,
+                Shader.TileMode.REPEAT);
+        Bitmap dest = Bitmap.createBitmap(mBackgroundBitmap.getWidth(), mBackgroundBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        if (dest == null)
+            return;
+        Canvas c = new Canvas(dest);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setShader(mBitmapShader);
+        c.drawCircle(mCenterX, mCenterY - viewTopChange, mRadius, paint);
+        canvas.drawBitmap(dest, 0, 0, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (isAnimated)
+            return super.onTouchEvent(event);
+        if (mVelocityTracker == null)
+            mVelocityTracker = VelocityTracker.obtain();
+        mVelocityTracker.addMovement(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downPointId = event.getPointerId(0);
+                downX = lastX = (int) event.getX();
+                downY = (int) event.getY();
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                return true;
+            case MotionEvent.ACTION_UP:
+                return true;
+
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public interface SignAble {
+
     }
 
     private class SignView {
@@ -132,12 +198,12 @@ public class SlidingArcView extends ViewGroup {
          */
         private void initView() {
             centerX = (width) / 2 + width * index;
-            centerY = CentY + (int) Math.sqrt(Math.pow(RADIUS, 2) - (int) Math.pow((centerX - CentX), 2));
+            centerY = mCenterY + (int) Math.sqrt(Math.pow(mRadius, 2) - (int) Math.pow((centerX - mCenterX), 2));
         }
 
         public void scroll(int scrollX) {
             this.centerX += scrollX;
-            centerY = CentY + (int) Math.sqrt(Math.pow(RADIUS, 2) - Math.pow((centerX - CentX), 2));
+            centerY = mCenterY + (int) Math.sqrt(Math.pow(mRadius, 2) - Math.pow((centerX - mCenterX), 2));
         }
 
         public int getCenterX() {
@@ -160,7 +226,7 @@ public class SlidingArcView extends ViewGroup {
                     centerX + size / 2,
                     centerX + size / 2 - viewTopChange);
             // 以是否靠近中心点 来判断是否变大变小
-            if (centerX >= CentX && centerX - CentX <= width) {
+            if (centerX >= mCenterX && centerX - mCenterX <= width) {
                 // 屏幕右半部分移动运动 变小
                 float scale = (float) (centerX - width - width / 2) / (float) width - 1.0f;
                 ViewCompat.setScaleX(view, normalScale + maxScale - maxScale * scale);
@@ -168,7 +234,7 @@ public class SlidingArcView extends ViewGroup {
                 if (scale >= 0.5f) {
                     isChoose = false;
                 }
-            } else if (centerX <= CentX && CentX - centerX <= width) {
+            } else if (centerX <= mCenterX && mCenterX - centerX <= width) {
                 // 屏幕左半部分移动 变大
                 float scale = (float) (centerX - width - width / 2) / (float) width;
                 ViewCompat.setScaleX(view, normalScale + maxScale * scale);
@@ -210,7 +276,7 @@ public class SlidingArcView extends ViewGroup {
          * 改变Y点坐标
          */
         public void changeY() {
-            centerY = CentY + (int) Math.sqrt(Math.pow(RADIUS, 2));
+            centerY = mCenterY + (int) Math.sqrt(Math.pow(mRadius, 2));
         }
 
         public boolean noLeftView() {
@@ -221,4 +287,5 @@ public class SlidingArcView extends ViewGroup {
             return centerX + width / 2 + width / 2 < ScreenUtil.getScreenW();
         }
     }
+
 }

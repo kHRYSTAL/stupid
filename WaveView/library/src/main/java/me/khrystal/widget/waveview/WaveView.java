c9278c08c26abf01877b1bb5ca041fc06e428678
kHRYSTAL <723526676@qq.com>
2018-08-03 17:51:03 +0800

    feat[ALL]:update waveview project

---
+++ WaveView/library/src/main/java/me/khrystal/widget/waveview/WaveView.java
@@ -0,0 +1,82 @@
+package me.khrystal.widget.waveview;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.BitmapShader;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * usage:
+ * author: kHRYSTAL
+ * create time: 18/8/3
+ * update time:
+ * email: 723526676@qq.com
+ */
+public class WaveView extends View {
+
+    private static final float DEFAULT_AMPLITUDE_RATIO = 0.05f;
+    private static final float DEFAULT_WATER_LEVEL_RATIO = 0.5f;
+    private static final float DEFAULT_WAVE_LENGTH_RATIO = 1.0f;
+    private static final float DEFAULT_WAVE_SHIFT_RATIO = 0.0f;
+
+    public static final int DEFAULT_BEHIND_WAVE_COLOR = Color.parseColor("#28FFFFFF");
+    public static final int DEFAULT_FRONT_WAVE_COLOR = Color.parseColor("#3CFFFFFF");
+    public static final ShapeType DEFAULT_WAVE_SHAPE = ShapeType.CIRCLE;
+
+    public enum ShapeType {
+        CIRCLE,
+        SQUARE
+    }
+
+    private boolean mShowWave;
+
+    private BitmapShader mWaveShader;
+    private Matrix mShaderMatrix;
+    private Paint mViewPaint;
+    private Paint mBorderPaint;
+
+    private float mDefaultAmplitude;
+    private float mDefaultWaterLevel;
+    private float mDefaultLength;
+    private double mDefaultAngularFrequency;
+
+    private float mAmplitudeRatio = DEFAULT_AMPLITUDE_RATIO;
+    private float mWaveLengthRatio = DEFAULT_WAVE_LENGTH_RATIO;
+    private float mWaterLevelRatio = DEFAULT_WATER_LEVEL_RATIO;
+    private float mWaveShiftRatio = DEFAULT_WAVE_SHIFT_RATIO;
+
+    private int mBehindWaveColor = DEFAULT_BEHIND_WAVE_COLOR;
+    private int mFrontWaveColor = DEFAULT_FRONT_WAVE_COLOR;
+    private ShapeType mShapeType = DEFAULT_WAVE_SHAPE;
+
+    public WaveView(Context context) {
+        super(context);
+        init();
+    }
+
+    public WaveView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public WaveView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mShaderMatrix = new Matrix();
+        mViewPaint = new Paint();
+        mViewPaint.setAntiAlias(true);
+    }
+
+    public float getWaveShiftRatio() {
+        return mWaveShiftRatio;
+    }
+}

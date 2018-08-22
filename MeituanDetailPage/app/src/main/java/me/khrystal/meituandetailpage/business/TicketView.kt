256e70598e699bb47a1a2f33056f0ef78a73366a
kHRYSTAL <723526676@qq.com>
2018-08-22 18:32:17 +0800

    update TicketView

--- MeituanDetailPage/app/src/main/java/me/khrystal/meituandetailpage/business/TicketView.kt
+++ MeituanDetailPage/app/src/main/java/me/khrystal/meituandetailpage/business/TicketView.kt
@@ -1,12 +1,14 @@
 package me.khrystal.meituandetailpage.business
 
-import android.animation.Animator
+import android.animation.ValueAnimator
 import android.content.Context
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import kotlinx.android.synthetic.main.ticket_view.view.*
 import me.khrystal.meituandetailpage.R
+import me.khrystal.meituandetailpage.view.*
+import me.khrystal.meituandetailpage.text
 
 /**
  *
@@ -16,7 +18,7 @@ import me.khrystal.meituandetailpage.R
  * update time:
  * email: 723526676@qq.com
  */
-class TicketView(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs), Animator.AnimatorListener {
+class TicketView(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs), AnimationUpdateListener {
 
     private var firstLayout: Boolean = false
 
@@ -28,24 +30,25 @@ class TicketView(context: Context, attrs: AttributeSet?) : ConstraintLayout(cont
         super.onWindowFocusChanged(hasWindowFocus)
         if (!firstLayout) {
             firstLayout = true
-//            TODO()
+            vBorder1.stateSave(R.id.vs1).a(1F)
+            vBorder1.stateSave(R.id.vs2).ws(3.8F).a(0F)
+            vBorder2.stateSave(R.id.vs1).a(0F)
+            vBorder2.stateSave(R.id.vs2).ws(3.8F).a(1F)
+            vSimple.stateSave(R.id.vs1)
+            vSimple.stateSave(R.id.vs2).a(0F)
+            layDetail.stateSave(R.id.vs1)
+            layDetail.stateSave(R.id.vs2).sx(1F).sy(1F).a(1F)
         }
     }
 
-    override fun onAnimationCancel(p0: Animator?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun set(amount: Int, limit: Int, expireTime: String) {
+        vSimple.text("领￥$amount")
+        vDetail1.text("￥$amount")
+        vDetail2.text("满$limit 可用")
+        vDetail3.text("有效期至$expireTime")
     }
 
-    override fun onAnimationEnd(p0: Animator?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onAnimationUpdate(tag1: Int, tag2: Int, p: Float) {
+        arrayOf(vBorder1, vBorder2, vSimple, layDetail).forEach { it.stateRefresh(tag1, tag2, p) }
     }
-
-    override fun onAnimationStart(p0: Animator?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onAnimationRepeat(p0: Animator?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
 }

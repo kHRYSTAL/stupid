ad66e045eff28be17a44a2c2f179f3b7c60c8944
kHRYSTAL <723526676@qq.com>
2019-01-15 12:43:48 +0800

    feat[textselection]: add text selection

---
+++ TextSelection/app/src/main/java/khrystal/me/textselection/selection/Selectable.java
@@ -0,0 +1,23 @@
+package khrystal.me.textselection.selection;
+
+/**
+ * Created by a.kapitonov on 22.01.2015.
+ */
+public interface Selectable {
+    int getOffsetForPosition(int x, int y);
+    int getVisibility();
+    CharSequence getText();
+    void setText(CharSequence text);
+    void getLocationOnScreen(int[] location);
+    int getHeight();
+    int getWidth();
+    float[] getPositionForOffset(int offset, float[] position);
+    void selectText(int start, int end);
+    CharSequence getSelectedText();
+    boolean isInside(int evX, int evY);
+    void setColor(int selectionColor);
+    int getStartSelection();
+    int getEndSelection();
+    String getKey();
+    void setKey(String key);
+}

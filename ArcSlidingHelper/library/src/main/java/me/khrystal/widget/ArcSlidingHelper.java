255bc1ad11151516dc6f7373627a04643547e148
kHRYSTAL <723526676@qq.com>
2019-03-22 19:29:30 +0800

    feat[ArcSlidingHelper]: init project

---
+++ ArcSlidingHelper/library/src/main/java/me/khrystal/widget/ArcSlidingHelper.java
@@ -0,0 +1,11 @@
+package me.khrystal.widget;
+
+/**
+ * usage:
+ * author: kHRYSTAL
+ * create time: 19/3/22
+ * update time:
+ * email: 723526676@qq.com
+ */
+public class ArcSlidingHelper {
+}
--- DoubleScrollView/app/src/main/java/me/khrystal/doublescrollview/MainActivity.java
+++ DoubleScrollView/app/src/main/java/me/khrystal/doublescrollview/MainActivity.java
@@ -1,13 +1,88 @@
 package me.khrystal.doublescrollview;
 
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import me.khrystal.fragment.CommentFragment;
+import me.khrystal.fragment.ContentDetailFragment;
+import me.khrystal.fragment.GoodsDetailFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabs;
+    private ViewPager viewPager;
+    private Toolbar toolbar;
+    private MiniPagerAdapter miniPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
         setContentView(R.layout.activity_main);
+        tabs = (TabLayout) findViewById(R.id.tabs);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.arrowback);
+        miniPagerAdapter = new MiniPagerAdapter(getSupportFragmentManager());
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.setAdapter(miniPagerAdapter);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+    public class MiniPagerAdapter extends FragmentPagerAdapter {
+
+        Fragment[] fragments = new Fragment[]{GoodsDetailFragment.newInstance(), ContentDetailFragment.newInstance(), CommentFragment.newInstance()};
+        String[] titles = new String[]{"商品", "详情", "评价"};
+
+        public MiniPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragments[i];
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_toggle) {
+            if (miniPagerAdapter.getItem(0) instanceof GoodsDetailFragment) {
+                ((GoodsDetailFragment) miniPagerAdapter.getItem(0)).toggle();
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

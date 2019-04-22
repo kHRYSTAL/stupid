7799b52e00d68e5881598bce2ce16791a62bf147
kHRYSTAL <723526676@qq.com>
2019-04-22 10:24:30 +0800

    feat[flipviewpager]: update FliViewPager, motionEvent not overwrite

--- FlipViewPager/library/src/main/java/me/khrystal/widget/flipviewpager/view/FlipViewPager.java
+++ FlipViewPager/library/src/main/java/me/khrystal/widget/flipviewpager/view/FlipViewPager.java
@@ -1,18 +1,20 @@
 package me.khrystal.widget.flipviewpager.view;
 
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Camera;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.animation.LinearInterpolator;
 import android.widget.EdgeEffect;
 import android.widget.FrameLayout;
 import android.widget.Scroller;
@@ -73,21 +75,144 @@ public class FlipViewPager extends FrameLayout {
 
     private OnChangePageListener onChangePageListener;
 
-    public FlipViewPager(@NonNull Context context) {
-        super(context);
-    }
-
     public FlipViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
-    public FlipViewPager(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    private void init() {
+        ViewConfiguration configuration = ViewConfiguration.get(getContext());
+        mScroller = new Scroller(getContext(), new LinearInterpolator());
+        mTouchSlop = configuration.getScaledPagingTouchSlop();
+        mMinimumVelocity = configuration.getScaledMinimumFlingVelocity();
+        mMaximumVelocity = configuration.getScaledMaximumFlingVelocity();
+        mLeftEdgeEffect = new EdgeEffect(getContext());
+        mRightEdgeEffect = new EdgeEffect(getContext());
+        mShadePaint.setColor(Color.BLACK);
+        mShinePaint.setColor(Color.WHITE);
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public FlipViewPager(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        mLeftRect.set(0, 0, getWidth() / 2, getHeight());
+        mRightRect.set(getWidth() / 2, 0, getWidth(), getHeight());
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        // flush data of canvas
+        if (!mScroller.isFinished() && mScroller.computeScrollOffset()) {
+            setFlipDistance(mScroller.getCurrY());
+        }
+
+        if (flipping || !mScroller.isFinished()) {
+            // Drawing prev half
+            canvas.save();
+            canvas.clipRect(mLeftRect);
+            PageItem leftPage = getDegressDone() >= 90 ? mPrev : mCurrent;
+            drawChild(canvas, leftPage.pageView, 0);
+            canvas.restore();
+            // Drawing next half
+            canvas.save();
+            canvas.clipRect(mRightRect);
+            PageItem rightPage = getDegressDone() >= 90 ? mCurrent : mNext;
+            drawChild(canvas, rightPage.pageView, 0);
+            canvas.restore();
+            // Drawing rotation
+            drawFlippingHalf(canvas);
+        } else {
+            mScroller.abortAnimation();
+            drawChild(canvas, mCurrent.pageView, 0);
+            if (onChangePageListener != null) {
+                onChangePageListener.onFlipped(mCurrentPageIndex);
+            }
+        }
+        if (drawEdges(canvas)) {
+            invalidate();
+        }
+    }
+
+    public void setOnChangePageListener(OnChangePageListener onChangePageListener) {
+        this.onChangePageListener = onChangePageListener;
+    }
+
+    private void drawFlippingHalf(Canvas canvas) {
+        canvas.save();
+        mCamera.save();
+        canvas.clipRect(getDegressDone() > 90 ? mLeftRect : mRightRect);
+        mCamera.rotateY(getDegressDone() > 90 ? 180 - getDegressDone() : -getDegressDone());
+        mCamera.getMatrix(mMatrix);
+
+        mMatrix.preScale(0.25f, 0.25f);
+        mMatrix.postScale(4.0f, 4.0f);
+        mMatrix.preTranslate(-getWidth() / 2, -getHeight() / 2);
+        mMatrix.postTranslate(getWidth() / 2, getHeight() / 2);
+
+        canvas.concat(mMatrix);
+        drawChild(canvas, mCurrent.pageView, 0);
+        drawFlippingShadeShine(canvas);
+        mCamera.restore();
+        canvas.restore();
+    }
+
+    private void drawFlippingShadeShine(Canvas canvas) {
+        if (getDegressDone() < 90) {
+            mShinePaint.setAlpha((int) (getDegressDone() / 90) * FLIP_SHADE_ALPHA);
+            canvas.drawRect(mRightRect, mShinePaint);
+        } else {
+            mShinePaint.setAlpha((int) ((Math.abs(getDegressDone() - 180) / 90f) * FLIP_SHADE_ALPHA));
+            canvas.drawRect(mLeftRect, mShadePaint);
+        }
+    }
+
+    private float getDegressDone() {
+        float flipDistance = mFlipDistance % FLIP_DISTANCE;
+        if (flipDistance < 0) {
+            flipDistance += FLIP_DISTANCE;
+        }
+        return (flipDistance / FLIP_DISTANCE) * 180;
+    }
+
+    public boolean drawEdges(Canvas canvas) {
+        boolean shouldContinue = false;
+        boolean rightNotFinished = !mRightEdgeEffect.isFinished();
+        if (rightNotFinished || !mLeftEdgeEffect.isFinished()) {
+            canvas.save();
+            mRightEdgeEffect.setSize(getHeight(), getWidth());
+            canvas.rotate(rightNotFinished ? 270 : 90);
+            canvas.translate(rightNotFinished ? -getHeight() : 0, rightNotFinished ? 0 : -getWidth());
+            shouldContinue = rightNotFinished ? mRightEdgeEffect.draw(canvas) : mLeftEdgeEffect.draw(canvas);
+            canvas.restore();
+        }
+        return shouldContinue;
+    }
+
+    // update data of flip view
+    private void setFlipDistance(float flipDistance) {
+        if (flipDistance == mFlipDistance) return;
+        mFlipDistance = flipDistance;
+        int currentPageIndex = Math.round(mFlipDistance / FLIP_DISTANCE);
+        if (mCurrentPageIndex != currentPageIndex) {
+            mCurrentPageIndex = currentPageIndex;
+            recycleActiveViews();
+            if (mCurrentPageIndex > 0) {
+                mPrev.fill(mCurrentPageIndex - 1);
+            }
+            if (mCurrentPageIndex >= 0 && mCurrentPageIndex < mPageCount) {
+                mCurrent.fill(mCurrentPageIndex);
+            }
+            if (mCurrentPageIndex < mPageCount - 1) {
+                mNext.fill(mCurrentPageIndex + 1);
+            }
+            invalidate();
+        }
+    }
+
+    private void recycleActiveViews() {
+        mPrev.recycle();
+        mCurrent.recycle();
+        mNext.recycle();
     }
 
     // Internal interface to store page position

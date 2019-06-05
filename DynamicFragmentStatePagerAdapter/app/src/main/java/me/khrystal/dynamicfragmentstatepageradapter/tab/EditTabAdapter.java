8c681868ff3b9a14489133d67f28316e2a787e2c
kHRYSTAL <723526676@qq.com>
2019-06-05 11:29:25 +0800

    feat[dynamicFragmentPagerAdapter]: add pojo

---
+++ DynamicFragmentStatePagerAdapter/app/src/main/java/me/khrystal/dynamicfragmentstatepageradapter/tab/EditTabAdapter.java
@@ -0,0 +1,30 @@
+package me.khrystal.dynamicfragmentstatepageradapter.tab;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * usage:
+ * author: kHRYSTAL
+ * create time: 19/6/5
+ * update time:
+ * email: 723526676@qq.com
+ */
+public class EditTabAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}

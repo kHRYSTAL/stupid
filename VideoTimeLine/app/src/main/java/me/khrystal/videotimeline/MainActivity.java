f34b5005739757fbdc43210b347bb818f83c18e9
kHRYSTAL <723526676@qq.com>
2020-12-01 16:33:34 +0800

    add[VideoTimeLine]:init project

---
+++ VideoTimeLine/app/src/main/java/me/khrystal/videotimeline/MainActivity.java
@@ -0,0 +1,14 @@
+package me.khrystal.videotimeline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
